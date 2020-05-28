a26beeed95c612c7dc7a8c4c9cd878ccabc89fc0
Rupank Bansal <rubansa@microsoft.com>
2020-05-29 05:02:29 +0530

    Added Filters before Step2 and Step 4,5

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,63 +1,127 @@
 import * as _ from "lodash";
 import { Link } from 'react-router-dom';
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getAllData, getError, getPending } from '../reducers/productReducer';
 import { parseData, applyRootFilter } from '../actions/productActions';
 import Button from '@material-ui/core/Button';
+import ToggleButton from '../components/ToggleButton';
 import './homepage.css';
-class HomePage extends Component {
 
-    constructor(props) {
-        super(props)
-        this.shouldComponentRender = this.shouldComponentRender.bind(this);
+function HomePage(props) {
+    const { data, error, applyRootFilter, isLoading } = props;
+    const [buttonStatus, setButtonVisibility] = useState({
+        'Step 1': true,
+        'Step 2': false,
+        'Step 3': true,
+        'Step 4': false,
+        'Step 5': false
+    })
+
+    const [radioChecked, setRadioCheck] = useState({
+        'Step 2 Check': false,
+        'Step 4 Check': false
+    });
+
+    const checkButtonLabels = {
+        'Step 2': 'Battery',
+        'Step 4': 'Test Drive'
     }
 
-    render() {
-        const { data, error, applyRootFilter } = this.props;
+    useEffect(() => {
+        const keys = _.keys(radioChecked);
+        let obj = {};
+        for (let key of keys) {
+            if (key === 'Step 2 Check') {
+                obj['Step 2'] = radioChecked[key]
 
-        const handleClick = (index) => {
-            applyRootFilter(index, data);
+            }
+            else if (key === 'Step 4 Check') {
+                // if (radioChecked[key]) {
+                obj['Step 4'] = radioChecked[key];
+                obj['Step 5'] = radioChecked[key];
+                // }
+            }
+        }
+        setButtonVisibility({ ...buttonStatus, ...Object.assign(buttonStatus, obj) });
+    }, [radioChecked])
+
+    const handleClick = (index) => {
+        applyRootFilter(index, data);
+    }
+
+    const shouldDisplayToggle = (index) => {
+        if (index === 'Step 2' || index === 'Step 4') {
+            return true;
+        }
+        return false;
+    }
+
+    const handleClickToggle = (index) => {
+        let toggleKey = null;
+        if (index === 'Step 2') {
+            toggleKey = 'Step 2 Check';
         }
 
-        if (!this.shouldComponentRender()) return (
-            <div>Loading...</div>
-        )
-        if (error) return (
-            <div>
-                {error}
-            </div>
-        )
-        const keys = _.keys(data);
-        return (
-            <div className="stepsParent">
-                {_.values(data).map((item, index) => (
-                    <div className='stepTile' key={keys[index]} onClick={(e) => handleClick(keys[index])}>
-                        <Link to={{
-                            pathname: `/${keys[index].replace(" ","")}/`,
-                            // state: {
-                            //     id: keys[index]
-                            // }
-                        }}>
-                            <Button className='stepTileBtn' variant="contained"> {keys[index]}</Button>
-                        </Link>
-                    </div>
+        else if (index === 'Step 4') {
+            toggleKey = 'Step 4 Check';
+        }
 
-                ))}
-            </div>
-        )
+        if (toggleKey !== null) {
+            let obj = {};
+            obj[toggleKey] = !radioChecked[toggleKey];
+            setRadioCheck({ ...radioChecked, ...Object.assign(radioChecked, obj) })
+        }
     }
 
-    shouldComponentRender() {
-        const { isLoading } = this.props;
+    const shouldComponentRender = () => {
         if (isLoading) {
             return false;
         } else {
             return true;
         }
     }
+
+    if (!shouldComponentRender()) return (
+        <div>Loading...</div>
+    )
+    if (error) return (
+        <div>
+            {error}
+        </div>
+    )
+    const keys = _.keys(data);
+    return (
+        <div className="stepsParent">
+            {_.values(data).map((item, index) => (
+                <div className="stepsContainer" key={index}>
+                    {shouldDisplayToggle(keys[index]) &&
+                        <div key={`${keys[index]}_radio`} className="customCheckSteps" onClick={(e) => handleClickToggle(keys[index])}>
+                            <ToggleButton item={checkButtonLabels[keys[index]]} />
+                        </div>}
+
+                    {buttonStatus[keys[index]] && <div className='stepTile' key={keys[index]} onClick={(e) => handleClick(keys[index])}>
+
+                        <Link to={{
+                            pathname: `/${keys[index].replace(" ", "")}/`
+                        }}>
+                            <Button className='stepTileBtn' variant="contained" > {keys[index]}</Button>
+                        </Link>
+                    </div>}
+                    {!buttonStatus[keys[index]] && <div className='stepTile' key={keys[index]}>
+
+                        <Button className='stepTileBtn' variant="contained" disabled> {keys[index]}</Button>
+                    </div>}
+                </div>
+
+            ))
+            }
+        </div >
+    )
 }
+
+
 const mapStateToProps = (state) => ({
     data: getAllData(state),
     error: getError(state),
