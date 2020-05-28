0fc188ffeac50bc18df30a8873382de1a2e0b301
Rupank Bansal <rubansa@microsoft.com>
2020-05-29 05:03:02 +0530

    Added Refurb Items Info

--- src/components/SliderWithLabelDummy.js
+++ src/components/SliderWithLabelDummy.js
@@ -2,18 +2,30 @@ import React, { useState } from 'react'
 import Switch from '@material-ui/core/Switch';
 import Button from '@material-ui/core/Button';
 import CheckBoxWithLabel from './CheckBoxWithLabel';
+import * as _ from 'lodash';
 import '../pages/homepage.css';
 
 function SliderWithLabelDummy(props) {
-    const { item, children } = props;
+    const { item, children, childrenValues } = props;
+
     const [checked, setChecked] = useState(false);
-    const [checkBoxChecked, setCheckBox] = useState(false);
+    const [suggestedRefurbs, setRefurbs] = useState({});
     const handleChange = (event) => {
         setChecked(!checked);
     };
-
-    const handleChangeCheckBox = (event) => {
-        setCheckBox(!checkBoxChecked);
+    const handleClickCheckBox = (val, checkBoxLabel, actions, index) => {
+        if (actions) {
+            actions = actions[index][0];
+            if (actions['RF Action Node 2']) {
+                if (val) {
+                    let obj = {};
+                    obj[checkBoxLabel] = actions['RF Action Node 2'];
+                    setRefurbs({ ...Object.assign(suggestedRefurbs, obj) });
+                } else {
+                    setRefurbs(_.omit(suggestedRefurbs, checkBoxLabel));
+                }
+            }
+        }
     };
 
     if (children && children.length > 0) {
@@ -25,7 +37,6 @@ function SliderWithLabelDummy(props) {
                     </div>
                     < Switch
                         checked={checked}
-                        // onChange={handleChange}
                         color="primary"
                         name="checked"
                         inputProps={{ 'aria-label': 'primary checkbox' }}
@@ -33,17 +44,23 @@ function SliderWithLabelDummy(props) {
                 </div>
             </Button>
             <div className="checkBoxParent">
-                {checked && children.map(child =>
-                    <SliderWithLabelDummy key={child} item={child} />)}
+                {checked && children.map((child, index) =>
+                    <CheckBoxWithLabel key={child} label={child} handleClicked={(val) => {
+                        handleClickCheckBox(val, child, childrenValues, index)
+                    }} />
+                )}
+                {checked && _.values(suggestedRefurbs).length > 0 &&
+                    <div className="checkBoxContainer">
+                        <div className="refurbParent">
+                            <div className = "refurbHeading">Suggested Refurb</div>
+                            <div className="refurbValues">{_.values(suggestedRefurbs).join(", ")}</div>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     }
-    else {
-        return (
-            <CheckBoxWithLabel key={item} label={item} />
-        )
-    }
-
+    return null;
 }
 
 export default SliderWithLabelDummy;
---
+++ src/components/ToggleButton.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Switch from '@material-ui/core/Switch';
+import Button from '@material-ui/core/Button';
+import '../pages/homepage.css';
+
+function ToggleButton(props) {
+    const { item } = props;
+    const [checked, setChecked] = useState(false);
+    const handleChange = (event) => {
+        setChecked(!checked);
+    };
+    return (
+        <Button className='stepTileBtn' variant="contained" onClick={handleChange}>
+            <div>
+                <div className="sliderLable">
+                    {item}
+                </div>
+                < Switch
+                    checked={checked}
+                    color="primary"
+                    name="checked"
+                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+            </div>
+        </Button>
+    )
+}
+
+export default ToggleButton
